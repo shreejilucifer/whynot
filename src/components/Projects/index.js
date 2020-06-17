bb4775e9e4269ababd1ca63ff249f81366d1e705
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-06-17 23:34:54 +0530

    Applications

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,92 +1,81 @@
-import { ProjectsContainer, Project } from '../../styles/Projects';
+import {
+	ProjectsContainer,
+	Project,
+	Applications,
+	NewAppBtn,
+} from '../../styles/Projects';
+import { TableHeader, TableRow } from '../../styles/Content';
+import { useState } from 'react';
 
 export default function ProjectsContent() {
 	return (
 		<ProjectsContainer>
-			<Project>
-				<div>
-					<span>{'>'}</span>
-				</div>
-				<div>
-					<span>Sappi</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Applications</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Tests/BM</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Open Vulnerabilities</span>
-				</div>
-				<div></div>
-			</Project>
-			<Project>
-				<div>
-					<span>{'>'}</span>
-				</div>
-				<div>
-					<span>Sappi</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Applications</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Tests/BM</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Open Vulnerabilities</span>
-				</div>
-				<div></div>
-			</Project>
-			<Project>
-				<div>
-					<span>{'>'}</span>
-				</div>
-				<div>
-					<span>Sappi</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Applications</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Tests/BM</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Open Vulnerabilities</span>
-				</div>
-				<div></div>
-			</Project>
-			<Project>
-				<div>
-					<span>{'>'}</span>
-				</div>
-				<div>
-					<span>Sappi</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Applications</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Tests/BM</span>
-				</div>
-				<div>
-					<span>0</span>
-					<span>Open Vulnerabilities</span>
-				</div>
-				<div></div>
-			</Project>
+			<Item />
 		</ProjectsContainer>
 	);
 }
+
+// Use Numbers for multiple open
+
+const Item = () => {
+	const [open, setOpen] = useState(false);
+	return (
+		<React.Fragment>
+			<Project open={open} onClick={() => setOpen(!open)}>
+				<div>
+					<span>
+						<img src="/img/down-arrow.svg" alt="Applications" />
+					</span>
+				</div>
+				<div>
+					<span>Sappi</span>
+				</div>
+				<div>
+					<span>0</span>
+					<span>Applications</span>
+				</div>
+				<div>
+					<span>0</span>
+					<span>Tests/BM</span>
+				</div>
+				<div>
+					<span>0</span>
+					<span>Open Vulnerabilities</span>
+				</div>
+				<div></div>
+			</Project>
+			{open && (
+				<Applications>
+					<TableHeader span="25% 25% 15% 10% 10% 10% auto">
+						<span>Application Name</span>
+						<span>URL</span>
+						<span>Test in progress</span>
+						<span>T/BM</span>
+						<span>Bugs</span>
+						<span>Score</span>
+					</TableHeader>
+					<TableRow span="25% 25% 15% 10% 10% 10% auto">
+						<span>Sappi XYZ</span>
+						<span>sappi.xyz</span>
+						<span>Testing</span>
+						<span>1</span>
+						<span>8</span>
+						<span>9/10</span>
+					</TableRow>
+					<TableRow span="25% 25% 15% 10% 10% 10% auto">
+						<span>Sappi XYZ</span>
+						<span>sappi.xyz</span>
+						<span>Testing</span>
+						<span>1</span>
+						<span>8</span>
+						<span>9/10</span>
+					</TableRow>
+					<NewAppBtn>
+						<span>+</span>
+						<span>Add New Application</span>
+					</NewAppBtn>
+				</Applications>
+			)}
+		</React.Fragment>
+	);
+};
--- src/styles/Projects.js
+++ src/styles/Projects.js
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const ProjectsContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+	min-height: 60vh;
 `;
 
 export const Project = styled.div`
@@ -10,14 +11,23 @@ export const Project = styled.div`
 	width: 100%;
 	display: grid;
 	grid-template-columns: 10% 35% 15% 15% 15% 10%;
-	background-color: ${(props) => props.theme.colors.white};
+	${(props) =>
+		props.open
+			? css`
+					background-color: ${(props) => props.theme.colors.secondary};
+					& span {
+						color: ${(props) => props.theme.colors.white};
+					}
+			  `
+			: css`
+					background-color: ${(props) => props.theme.colors.white};
+			  `}
 	padding-top: 2em;
 	padding-bottom: 2em;
 
 	&:hover {
 		cursor: pointer;
 		background-color: ${(props) => props.theme.colors.secondary};
-
 		& span {
 			color: ${(props) => props.theme.colors.white};
 		}
@@ -31,11 +41,67 @@ export const Project = styled.div`
 
 		&:first-child {
 			align-items: center;
+
+			& img {
+				height: 1em;
+				width: 2em;
+				${(props) =>
+					props.open
+						? css`
+								transform: rotate(0deg);
+						  `
+						: css`
+								transform: rotate(-90deg);
+						  `}
+			}
 		}
 
 		& > span {
 			font-size: 1.2rem;
 			font-family: ${(props) => props.theme.fonts.primary};
 		}
+
+		&:nth-child(3),
+		&:nth-child(4),
+		&:nth-child(5) {
+			& > span:first-child {
+				font-size: 2rem;
+			}
+			& > span:last-child {
+				font-size: 0.8rem;
+			}
+		}
 	}
+`;
+
+export const Applications = styled.div`
+	background-color: ${(props) => props.theme.colors.white};
+	border: 1px solid ${(props) => props.theme.colors.white};
+`;
+
+export const NewAppBtn = styled.button`
+	border: 2px solid ${(props) => props.theme.colors.primary};
+	border-radius: 0.2em;
+	background: none;
+
+	&:hover {
+		cursor: pointer;
+		background-color: ${(props) => props.theme.colors.primary};
+		color: ${(props) => props.theme.colors.white};
+	}
+
+	& > span {
+		margin-left: 1em;
+		font-family: ${(props) => props.theme.fonts.primary};
+	}
+	justify-self: center;
+	margin-left: auto;
+	margin-right: auto;
+	margin-top: 2em;
+	margin-bottom: 2em;
+	padding: 1%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
 `;
